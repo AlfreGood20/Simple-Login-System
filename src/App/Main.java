f925dce3933712d89da7ab876ee69deeab883949
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2025-01-16 18:35:14 -0600

    Aqui estan las clases y ventanas de la app

---
+++ src/App/Main.java
@@ -0,0 +1,11 @@
+package App;
+import AppVentanaLogin.Ventana;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Ventana ventanita=new Ventana();
+		ventanita.setVisible(true);
+	}
+
+}

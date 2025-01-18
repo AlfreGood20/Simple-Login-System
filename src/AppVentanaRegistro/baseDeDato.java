a1c331f6717a092af3f7d651365ed8fc6fd974d4
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2025-01-18 15:51:12 -0600

    Agregue y agregue como un tipo de datos para que funcione el login

--- src/AppVentanaRegistro/baseDeDato.java
+++ src/AppVentanaRegistro/baseDeDato.java
@@ -1,5 +1,29 @@
 package AppVentanaRegistro;
 
-public class baseDeDato {
+import java.util.ArrayList;
 
+public class baseDeDato {
+	private static ArrayList<usuario> usuario;
+
+    static {
+        usuario = new ArrayList<>(); // Inicializa el ArrayList
+    }
+    
+    public baseDeDato() {
+    }
+
+    public void agregarUsuario(usuario datos) {
+        usuario.add(datos);
+    }
+
+    public static boolean darAcesso(String correo, String contraseña) {
+    	boolean encontrado = false;
+        for (int i = 0; i < usuario.size(); i++) {
+            if(usuario.get(i).getCorreo().equals(correo) && usuario.get(i).getContraseña().equals(contraseña)) {
+            	encontrado=true;
+            	break;
+            }
+        }
+        return encontrado;
+    }
 }

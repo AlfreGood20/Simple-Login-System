27886f00cc0e0120f800e239210a2fec3d6331d8
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2025-01-19 17:25:10 -0600

    Agregue un metodo para hacer mas optimo la ventana

--- src/AppVentanaRegistro/MetodoSueltos.java
+++ src/AppVentanaRegistro/MetodoSueltos.java
@@ -3,6 +3,7 @@ package AppVentanaRegistro;
 import java.awt.Color;
 import java.awt.Font;
 
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 public class MetodoSueltos {
@@ -11,4 +12,10 @@ public class MetodoSueltos {
 		campo.setDisabledTextColor(Color.BLACK);
 		campo.setFont(new Font("Arial", Font.BOLD,15));
 	}
+	
+	public static JLabel crearEtiqueta(String texto, int x, int y, int width, int height) {
+        JLabel etiqueta = new JLabel(texto);
+        etiqueta.setBounds(x, y, width, height);
+        return etiqueta;
+    }
 }
